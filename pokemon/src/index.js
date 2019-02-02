3961fe57830c553647884b54d834dbab28a143ca
april <martinezapril56@yahoo.com>
2019-02-02 14:22:02 -0500

    done

--- pokemon/src/index.js
+++ pokemon/src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import Page from './containers/pages'
-import './style/main.css'
-import configureStore from './redux/store/configure-store'
+import Page from './containers/pages';
+import './style/main.css';
+import configureStore from './redux/store/configure-store';
 
 const store = configureStore()
 
